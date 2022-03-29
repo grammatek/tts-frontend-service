7748452139d4e64bef6a4855b87d6d52651c4985
Anna Nikulásdóttir <anna@grammatek.com>
2022-03-29 14:04:55 +0000

    adapt paths in generate_proto

--- generate_proto.sh
+++ generate_proto.sh
@@ -2,12 +2,14 @@
 
 # Generates _pb2.py and _pb2_grpc.py for messages and services
 
-python3 -m venv service-venv
+# if venv does not yet exist, uncomment:
+#python3 -m venv service-venv
+
 source service-venv/bin/activate
 GRPC_TOOLS_FOUND=`pip list | grep grpcio-tools`
 if [ "$?" -ne 0  ]; then
 	pip install grpcio-tools
 fi
 
-python3 -m grpc_tools.protoc -I./proto/ --python_out=src/generated/ --grpc_python_out=src/generated/ proto/services/tts_frontend_service.proto
-python3 -m grpc_tools.protoc -I./proto/ --python_out=src/generated/ --grpc_python_out=src/generated/ proto/messages/tts_frontend_message.proto
+python3 -m grpc_tools.protoc -I./tts-frontend-proto/ --python_out=src/generated/ --grpc_python_out=src/generated/ tts-frontend-proto/services/text_preprocessing_service.proto
+python3 -m grpc_tools.protoc -I./tts-frontend-proto/ --python_out=src/generated/ --grpc_python_out=src/generated/ tts-frontend-proto/messages/text_preprocessing_message.proto
