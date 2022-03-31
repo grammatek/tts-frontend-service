417f010fe9300f6d0055e606304e5158602dfa17
Anna Nikulásdóttir <anna@grammatek.com>
2022-03-31 09:47:10 +0000

    remove install from generate-proto

--- generate_proto.sh
+++ generate_proto.sh
@@ -2,14 +2,7 @@
 
 # Generates _pb2.py and _pb2_grpc.py for messages and services
 
-# if venv does not yet exist, uncomment:
-#python3 -m venv service-venv
-
-source service-venv/bin/activate
-GRPC_TOOLS_FOUND=`pip list | grep grpcio-tools`
-if [ "$?" -ne 0  ]; then
-	pip install grpcio-tools
-fi
+# Make sure to have installed requirements before running this script
 
 python3 -m grpc_tools.protoc -I./tts-frontend-proto/ --python_out=src/generated/ --grpc_python_out=src/generated/ tts-frontend-proto/services/text_preprocessing_service.proto
 python3 -m grpc_tools.protoc -I./tts-frontend-proto/ --python_out=src/generated/ --grpc_python_out=src/generated/ tts-frontend-proto/messages/text_preprocessing_message.proto
