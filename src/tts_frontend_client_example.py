1f2cd911f3369c45720484834c39d03dee10a702
Anna Nikulásdóttir <anna@grammatek.com>
2022-04-08 21:53:36 +0000

    continue adaptation of new proto, add html clean

--- src/tts_frontend_client_example.py
+++ src/tts_frontend_client_example.py
@@ -6,8 +6,8 @@ sys.path.append(dirname(__file__)+'/generated/')
 import logging
 import grpc
 from google.protobuf import empty_pb2
-from generated.services import text_preprocessing_service_pb2_grpc
-from generated.messages import text_preprocessing_message_pb2 as msg_pb2
+from generated.services import preprocessing_service_pb2_grpc
+from generated.messages import preprocessing_message_pb2 as msg_pb2
 
 
 def get_version(stub):
@@ -31,24 +31,38 @@ def get_normalized_text(stub, text):
 
 def get_transcribed_text(stub, text):
     norm_domain = msg_pb2.NormalizationDomain(norm_domain=msg_pb2.NORM_DOMAIN_SPORT)
-    phoneme_descr = msg_pb2.PhonemeDescription(syllabified=True, stress_labels=True)
+    phoneme_descr = msg_pb2.PhonemeDescription(syllabified='.', stress_labels=True)
     message = msg_pb2.PreprocessRequest(content=text, domain=norm_domain, description=phoneme_descr)
     response = stub.Preprocess(message)
     print(response)
     print(response.processed_content)
 
 
+def get_html_string():
+    return '<p id="hix00274"><span id="qitl_0591" class="sentence">Í kjölfarið sýndi hann fram á að það stuðli að ' \
+               'heilbrigði ef einstaklingar geti fundið samhengi í tengslum við lífsatburði eða öðlast skilning á aðstæðum sínum. ' \
+               '</span><span id="qitl_0592" class="sentence">Hann taldi uppsprettu heilbrigðis ' \
+               '(e. </span><em><span id="qitl_0593" class="sentence">salutogenesis)</span></em><span id="qitl_0594" class="sentence"> ' \
+               'vera að finna í mismunandi hæfni einstaklinga til að stjórna viðbrögðum sínum við álagi. </span>' \
+               '<span id="qitl_0595" class="sentence">Antonovsky sýndi fram á að ef einstaklingar sem upplifðu álag sæju ' \
+               'tilgang með reynslu sinni, þá þróaðist með þeim tilfinning fyrir samhengi í lífinu ' \
+               '(e. </span><em><span id="qitl_0596" class="sentence">sense of coherence).</span></em>' \
+               '<span id="qitl_0597" class="sentence"> Sigrún Gunnarsdóttir hefur íslenskað skilgreiningu hugtaksins ' \
+               'um tilfinningu fyrir samhengi í lífinu á eftirfarandi hátt: </span></p>'
+
 def run():
     with grpc.insecure_channel('localhost:8080') as channel:
-        stub = text_preprocessing_service_pb2_grpc.TextPreprocessingStub(channel)
+        stub = preprocessing_service_pb2_grpc.PreprocessingStub(channel)
         print("-------------- GetVersion --------------")
         get_version(stub)
         print("-------------- Clean --------------")
         get_clean_text(stub, "en π námundast í 3.14")
-        print("-------------- Normalize --------------")
-        get_normalized_text(stub, "það voru 55 km eftir")
-        print("-------------- Transcribe --------------")
-        get_transcribed_text(stub, "það voru 55 km eftir, sögðu allir nema 1")
+        print("-------------- Clean HTML --------------")
+        get_clean_text(stub, get_html_string(), html=True)
+        #print("-------------- Normalize --------------")
+        #get_normalized_text(stub, "það voru 55 km eftir")
+        #print("-------------- Transcribe --------------")
+        #get_transcribed_text(stub, "það voru 55 km eftir, sögðu allir nema 1")
 
 
 if __name__=='__main__':
--- src/tts_frontend_server.py
+++ src/tts_frontend_server.py
@@ -8,23 +8,26 @@ sys.path.append(dirname(__file__)+'/generated/')
 from concurrent import futures
 import logging
 import grpc
+from google.protobuf import empty_pb2
 
-from generated.messages import text_preprocessing_message_pb2 as msg
-from generated.services import text_preprocessing_service_pb2_grpc as service
+from generated.messages import preprocessing_message_pb2 as msg
+from generated.services import preprocessing_service_pb2_grpc as service
 
 from manager.textprocessing_manager import Manager
 
 
-class TTSFrontendServicer(service.TextPreprocessingServicer):
-    """Provides methods that implement functionality of tts frontend server."""
+class TTSFrontendServicer(service.PreprocessingServicer):
+    """Provides methods that implement functionality of a TTS frontend pipeline.
+    For example usage see `tts_frontend_client_example.py` """
 
     def __init__(self):
         # init pipeline
         self.manager = Manager()
 
-    def init_clean_token(self, token: CleanToken) -> msg.CleanToken:
+    @staticmethod
+    def init_clean_token(token: CleanToken) -> msg.CleanToken:
         """
-
+        Initialize a cleanToken message from the cleanToken object parameter.
         """
         orig = token.original_token
         embedded_orig = msg.Token(name=orig.name, index=orig.token_index, span_from=orig.start, span_to=orig.end)
@@ -32,9 +35,10 @@ class TTSFrontendServicer(service.TextPreprocessingServicer):
 
         return clean_token
 
-    def init_norm_token(self, token: NormalizedToken) -> msg.NormalizedToken:
+    @staticmethod
+    def init_norm_token(token: NormalizedToken) -> msg.NormalizedToken:
         """
-
+        Initialize a normalizedToken message from the normalizedToken object parameter.
         """
         clean = token.clean_token
         orig = clean.original_token
@@ -44,9 +48,14 @@ class TTSFrontendServicer(service.TextPreprocessingServicer):
 
         return norm_token
 
-    def init_transcr_token(self, token: TranscribedToken) -> msg.TranscribedToken:
+    @staticmethod
+    def init_transcr_token(token: TranscribedToken) -> msg.TranscribedToken:
         """
-
+        Initialize a transcribedToken message from the transcribedToken object parameter.
+        A transcribedToken contains three embedded tokens: the original input token, the clean token,
+        the normalized token, and finally the transcribed token. The original, clean and normalized tokens
+        might be all identical (e.g. 'halló', 'halló', 'halló') but the transcribed token is represented in
+        a phonetic alphabet (e.g. 'h a l ou').
         """
         norm = token.normalized
         clean = norm.clean_token
@@ -59,16 +68,20 @@ class TTSFrontendServicer(service.TextPreprocessingServicer):
 
     def Clean(self, request: msg.TextCleanRequest, context) -> msg.TextCleanResponse:
         """Clean text, returns clean text without non-valid chars or symbols.
+        Status: the only parameter reachable form here is the parse_html, i.e. if request.parse_html == True
+        the input string (request.content) will be processed as html with text extracted, english labels
+        substituted by SSML-lang tags and HTML-tags either replaced by a dot or deleted.
+        TODO: add other parameters of the text cleaner to the interface
         """
         context.set_code(grpc.StatusCode.OK)
 
-        cleanRes = self.manager.clean(request.content)
+        clean_result = self.manager.clean(request.content, html=request.parse_html)
         response = msg.TextCleanResponse()
-        for token in cleanRes:
+        for token in clean_result:
 
             if isinstance(token, TagToken):
-                tagTok = msg.TagToken(name=token.name, index=token.token_index)
-                tok = msg.CleanTokenList(tag=tagTok)
+                tag_tok = msg.TagToken(name=token.name, index=token.token_index)
+                tok = msg.CleanTokenList(tag=tag_tok)
             else:
                 clean_token = self.init_clean_token(token)
                 tok = msg.CleanTokenList(cleaned=clean_token)
@@ -79,34 +92,39 @@ class TTSFrontendServicer(service.TextPreprocessingServicer):
         return response
 
     def Normalize(self, request: msg.NormalizeRequest, context) -> msg.NormalizedResponse:
-        """Normalize text for TTS, returns normalized text prepared for g2p
+        """Normalize text for TTS, returns normalized text prepared for g2p.
+        Accessible parameter is the normalization domain which can be set to msg.NORM_DOMAIN_SPORT if the
+        input text is known to have sport as subject. Causes '2-1' to be normalized to 'tvö <sil> eitt' instead of
+        'tvö til eitt' as in the default domain setting.
         """
         context.set_code(grpc.StatusCode.OK)
         if request.domain == msg.NORM_DOMAIN_SPORT:
             domain = 'sport'
         else:
             domain = ''
-        normalizedRes = self.manager.normalize(request.content, domain)
+        normalized_result = self.manager.normalize(request.content, domain)
         response = msg.NormalizedResponse()
-        for token in normalizedRes:
+        for token in normalized_result:
 
             if isinstance(token, TagToken):
-                tagTok = msg.TagToken(name=token.name, index=token.token_index)
-                tok = msg.NormalizedTokenList(tag=tagTok)
+                tag_token = msg.TagToken(name=token.name, index=token.token_index)
+                tok = msg.NormalizedTokenList(tag=tag_token)
             else:
                 norm_token = self.init_norm_token(token)
                 tok = msg.NormalizedTokenList(normalized=norm_token)
 
             response.tokens.append(tok)
             # don't add a tagToken's name to processed content string if the user doesn't want tags in the result string
-            if isinstance(token, TagToken) and msg.no_tag_tokens_in_content:
+            if isinstance(token, TagToken) and request.no_tag_tokens_in_content:
                 continue
             response.processed_content += token.name + ' '
 
         return response
 
     def Preprocess(self, request: msg.PreprocessRequest, context) -> msg.PreprocessedResponse:
-        """Preprocess text for TTS, including conversion to X-SAMPA
+        """Preprocess text for TTS, including conversion to X-SAMPA. Same settings for cleaning and normalizing
+        apply as described in Clean() and Normalize(), and additionally the following parameters can be
+        set:
         """
         context.set_code(grpc.StatusCode.OK)
         if request.domain == msg.NORM_DOMAIN_SPORT:
@@ -117,7 +135,7 @@ class TTSFrontendServicer(service.TextPreprocessingServicer):
         # add user dictionary, if present
 
         # add g2p settings, if present
-        self.manager.set_g2p_syllab_stress(request.description.syllabified)
+        self.manager.set_g2p_syllab_symbol(request.description.syllabified)
         transcribed_res = self.manager.transcribe(request.content, domain)
         response = msg.PreprocessedResponse()
         for token in transcribed_res:
@@ -131,17 +149,16 @@ class TTSFrontendServicer(service.TextPreprocessingServicer):
             response.tokens.append(tok)
 
             # don't add a tagToken's name to processed content string if the user doesn't want tags in the result string
-            if isinstance(token, TagToken) and msg.no_tag_tokens_in_content:
+            if isinstance(token, TagToken) and request.no_tag_tokens_in_content:
                 continue
 
             response.processed_content += token.name + ' '
 
         return response
 
-    def GetDefaultPhonemeDescription(self, request, context):
-        """Default values for the phoneme description
-        """
-        context.set_code(grpc.StatusCode.NOT_IMPLEMENTED)
+    def GetDefaultParameters(self, request, context) -> msg.DefaultProcessingResponse:
+        context.set_code(grpc.StatusCode.OK)
+        return msg.DefaultProcessingResponse
 
     def GetVersion(self, request, context):
         context.set_code(grpc.StatusCode.OK)
@@ -150,7 +167,7 @@ class TTSFrontendServicer(service.TextPreprocessingServicer):
 
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-    service.add_TextPreprocessingServicer_to_server(TTSFrontendServicer(), server)
+    service.add_PreprocessingServicer_to_server(TTSFrontendServicer(), server)
     server.add_insecure_port('[::]:8080')
     server.start()
     server.wait_for_termination()
